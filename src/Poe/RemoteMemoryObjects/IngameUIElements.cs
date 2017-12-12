437d1d024e42d508e04e24f86ebf389245c3cd65
Unknown <qvin0000@gmail.com>
2017-12-12 12:02:21 +0300

    updated chat msges element

--- src/Poe/RemoteMemoryObjects/IngameUIElements.cs
+++ src/Poe/RemoteMemoryObjects/IngameUIElements.cs
@@ -1,26 +1,20 @@
 using PoeHUD.Poe.Elements;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace PoeHUD.Poe.RemoteMemoryObjects
 {
     public class IngameUIElements : RemoteMemoryObject
     {
-        public Element FlaskPanel => ReadObjectAt<Element>(0xA30);
         public SkillBarElement SkillBar => ReadObjectAt<SkillBarElement>(0xB50);
-        public PoeChatElement ChatBox => ReadObjectAt<PoeChatElement>(0xBD0);
         public SkillBarElement HiddenSkillBar => ReadObjectAt<SkillBarElement>(0xB58);
+        public PoeChatElement ChatBox => GetObject<PoeChatElement>(M.ReadLong(Address + 0xBD0, 0xAB8, 0xC30));
         public Element QuestTracker => ReadObjectAt<Element>(0xC00);
         public Element OpenLeftPanel => ReadObjectAt<Element>(0xC40);
         public Element OpenRightPanel => ReadObjectAt<Element>(0xC48);
         public InventoryElement InventoryPanel => ReadObjectAt<InventoryElement>(0xC78);
         public Element TreePanel => ReadObjectAt<Element>(0xCA8);
         public Element AtlasPanel => ReadObjectAt<Element>(0xCB0);
-        public NpcDialog NpcDialog => ReadObjectAt<NpcDialog>(0xD68);
-        public VendorSellElement VendorPanel => ReadObjectAt<VendorSellElement>(0xD88);
         public Map Map => ReadObjectAt<Map>(0xD00);
-        //TODO need change to address + offests
-        public EntityLabel LeagueLabel => (Map.Children[2].Children[0].Children[2]).AsObject<EntityLabel>();
         public IEnumerable<ItemsOnGroundLabelElement> ItemsOnGroundLabels
         {
             get
@@ -31,29 +25,6 @@ namespace PoeHUD.Poe.RemoteMemoryObjects
         }
         public Element GemLvlUpPanel => ReadObjectAt<Element>(0xF18);
         public ItemOnGroundTooltip ItemOnGroundTooltip => ReadObjectAt<ItemOnGroundTooltip>(0xF80);
-        public Element Cursor => ReadObjectAt<Element>(0xB40);
-        
-        public List<EntityLabel> AllLabels => GetLabels();
-   
-        private List<EntityLabel> GetLabels()
-        {
-            Element labelsRoot = ReadObjectAt<Element>(0xD00);
-            Element allLabels = labelsRoot.ReadObjectAt<Element>(0xA08);
-            return allLabels.Children.Select(e => e.AsObject<EntityLabel>()).Where(entityLabel => entityLabel.Length > 0).ToList();
-        }
-
-
-        public Element PantheonPanel => ReadObjectAt<Element>(0xCC8);
-        public Element WaypointPanel => ReadObjectAt<Element>(0xCD8);
-        public Element CharInfoPanel => ReadObjectAt<Element>(0xCB0);
-        public Element CadiroOfferPanel => ReadObjectAt<Element>(0xDA0);
-        public Element CardTradePanel => ReadObjectAt<Element>(0xE08);
-        public Element LabChoicePanel => ReadObjectAt<Element>(0xDD8);
-        public Element TradePanel => ReadObjectAt<Element>(0xD90);
-        public Element InstancePanel => ReadObjectAt<Element>(0xE48);
-        public Element MapDevicePanel => ReadObjectAt<Element>(0xDC8);
-        public StashElement StashPanel => ReadObjectAt<StashElement>(0xC78);
-        
     }
 }
 
